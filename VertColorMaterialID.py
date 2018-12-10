9de35c413f8306ecdc8b76dbef5d6c912ca45667
OlesenJonas <45714731+OlesenJonas@users.noreply.github.com>
2018-12-10 20:05:37 +0100

    Update VertColorMaterialID.py

--- VertColorMaterialID.py
+++ VertColorMaterialID.py
@@ -5,12 +5,11 @@ bl_info = {
     "version": (1, 3),
     "blender": (2, 80, 0),
     "location": "Object Mode -> Toolbar",
-    "category": "Object",
-    "support": "COMMUNITY"
+    "category": "Object"
 }
 
 import bpy
-import random
+from bpy.props import *
 
 
 #########################################  END PRE STUFF #####################################################
@@ -31,11 +30,23 @@ class idTransfer(bpy.types.Operator):
         obj = context.object
         mesh = obj.data
 
+        #preset color palette
+        cols = [
+            [1.0,1.0,1.0],
+            [1.0,1.0,0.0],
+            [1.0,0.0,1.0],
+            [1.0,0.0,0.0],
+            [0.0,1.0,1.0],
+            [0.0,1.0,0.0],
+            [0.0,0.0,1.0],
+            [0.0,0.0,0.0]
+        ]
+
         #create vert color layer if not already
         if not mesh.vertex_colors:
             mesh.vertex_colors.new()
 
-        #Cancel if Object has no materials
+        #Cancel if object has no materials
         if len(obj.material_slots) == 0:
             bpy.context.window_manager.popup_menu(idTransfer.drawErr, title="No Material Data to transfer", icon='ERROR')
             return {"CANCELLED"}
@@ -43,23 +54,28 @@ class idTransfer(bpy.types.Operator):
         #iterate faces                                              
         for poly in mesh.polygons:
             mat = obj.material_slots[poly.material_index].material
-            rgb = mat.diffuse_color
             if mat is None:
                 bpy.context.window_manager.popup_menu(idTransfer.drawErr, title="No Material Data to transfer", icon='ERROR')
                 return {"CANCELLED"}
+            if context.scene.useViewpCol == True:
+                rgb = mat.diffuse_color
             else:
-                for loop_index in poly.loop_indices:
-                    mesh.vertex_colors.active.data[loop_index].color[0] = rgb[0]
-                    mesh.vertex_colors.active.data[loop_index].color[1] = rgb[1]
-                    mesh.vertex_colors.active.data[loop_index].color[2] = rgb[2]
+                rgb = cols[poly.material_index % 8]
+            for loop_index in poly.loop_indices:
+                mesh.vertex_colors.active.data[loop_index].color[0] = rgb[0]
+                mesh.vertex_colors.active.data[loop_index].color[1] = rgb[1]
+                mesh.vertex_colors.active.data[loop_index].color[2] = rgb[2]
 
-        #after transfer set viewport colors to white:
-        for mat in obj.material_slots:
-            mat.material.diffuse_color = [1.0,1.0,1.0]
+        if context.scene.resetAfter == True:
+            #after transfer set viewport colors to white:
+            for poly in mesh.polygons:
+                if obj.material_slots[poly.material_index].material.diffuse_color != [1.0,1.0,1.0]:
+                    obj.material_slots[poly.material_index].material.diffuse_color = [1.0,1.0,1.0]
 
         #at end switch to vertex paint to see change
         bpy.ops.object.mode_set(mode='VERTEX_PAINT')
-        bpy.context.window_manager.popup_menu(idTransfer.drawFin, title="Done!", icon='FILE_TICK')
+        if (context.scene.resetAfter == False) and (context.scene.useViewpCol == True):
+            bpy.context.window_manager.popup_menu(idTransfer.drawFin, title="Done!", icon='FILE_TICK')
         return {"FINISHED"}
 
     def drawErr(self, context):
@@ -83,6 +99,10 @@ class idTransferPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
+        scn = context.scene
+        
+        layout.prop(scn, "useViewpCol")
+        layout.prop(scn, "resetAfter")
 
         row = layout.row()
         row.operator("myops.add_idtransfer")
@@ -98,15 +118,25 @@ classes = (
 
 
 def register():
+
+    bpy.types.Scene.useViewpCol = BoolProperty(
+        name = "Use Viewport Colors", description="Uses Material Viewport Colors when true, otherwise generate Colors (up to 8)", default = True)
+    bpy.types.Scene.resetAfter = BoolProperty(
+        name = "Reset after Transfer", description="Clears viewport colors after transfer", default = True)
+
     from bpy.utils import register_class
     for cls in classes:
         register_class(cls)
 
 
 def unregister():
+
     from bpy.utils import unregister_class
     for cls in classes:
         unregister_class(cls)
+
+    del bpy.types.Scene.useViewpCol
+    del bpy.types.Scene.resetAfter
 
 
 if __name__ == '__main__':
